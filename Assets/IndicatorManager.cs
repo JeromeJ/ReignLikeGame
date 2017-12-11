c7be984afe3d0910579ed96fb8d28955d6664fa9
Technifutur <e-warning@hotmail.com>
2017-12-11 11:36:42 +0100

    Dynamic access to indicators via scripting

--- Assets/IndicatorManager.cs
+++ Assets/IndicatorManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IndicatorManager : MonoBehaviour
 {
     #region Public Members
+
+    public Image[] m_indicators;
+    public Transform m_transform;
 
     #endregion
 
@@ -14,12 +19,19 @@ public class IndicatorManager : MonoBehaviour
 
     #region System
 
-    void Start ()
+    private void Awake()
     {
-        
+        m_transform = GetComponent<Transform>();
+
+        GetIndicators();
+
+        foreach(Image indicator in m_indicators)
+        {
+            indicator.fillAmount = 0;
+        }
     }
 
-    void Update ()
+    private void Update ()
     {
         	
     }
@@ -27,6 +39,19 @@ public class IndicatorManager : MonoBehaviour
     #endregion
 
     #region Tools Debug and Utility
+
+    private void GetIndicators()
+    {
+        m_indicators = Enumerable.Range(0, m_transform.childCount).Select(
+            (n) => m_transform.GetChild(n).Find("Foreground").GetComponent<Image>()
+       ).ToArray();
+
+        // // Must use a List
+        // m_children = new List<Transform>();
+        //
+        // foreach(Transform child in m_transform)
+        //      m_children.Add(child);
+    }
 
     #endregion
 
